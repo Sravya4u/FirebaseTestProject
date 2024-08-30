444769ef58fa9f112539959b51a83700a28369da
Sravya <sravyachandrapati16@gmail.com>
2024-08-31 06:38:51 +1200

    Initial Commit

---
+++ FirebaseTestProject/FirebaseTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestProjectApp.swift
+//  FirebaseTestProject
+//
+//  Created by Sudheer Vasepalli on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
